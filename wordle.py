a18034e758d461399006f86efec211b76d1337d9
ValeOlivera <104220020+ValeOlivera@users.noreply.github.com>
2022-05-06 13:49:16 -0300

    Update wordle.py

--- wordle.py
+++ wordle.py
@@ -2,9 +2,12 @@ si  __nombre__  ==  '__principal__' :
  cantidad Palabaras  =  0
  archivo  =  abrir ( 'palabras.txt' ) #abre el archivo
  print ( '>>> FILTRANDO PALABRAS DE 5 LETRAS' )
+ diccionario  = []
  con  abierto ( 'palabras.txt' , 'r' ) como  archivo : #lee el archivo
     for  linea  in  archivo :     #recorro el archivo linea por linea
-        if  len ( linea ) ==  5 :   #condicional para verificar que la palabra se de tamaño 5 letras
+        if  len ( linea ) ==  6 :   #condicional para verificar que la palabra se de tamaño 5 letras
             cantidadPalabaras  +=  1
+            diccionario _ agregar ( linea . tira ())
  print ( '>>> OPERACION COMPLETADA CON SALIDA' )
  print ( '>>> SE ENCONTRARON '  +  str ( cantidadPalabaras ) +  ' PALABRAS DE 5 LETRAS' )  
+ imprimir ( diccionario )
