33e36c6455ab8d7268f609360c6a8312159eda47
ValeOlivera <104220020+ValeOlivera@users.noreply.github.com>
2022-05-13 13:25:28 -0300

    Update wordle.py

--- wordle.py
+++ wordle.py
@@ -11,3 +11,19 @@ si  __nombre__  ==  '__principal__' :
  print ( '>>> OPERACION COMPLETADA CON SALIDA' )
  print ( '>>> SE ENCONTRARON '  +  str ( cantidadPalabaras ) +  ' PALABRAS DE 5 LETRAS' )  
  imprimir ( diccionario )
+ palabraCorrecta = False # variable para repetir el loop/while
+ while not(palabraCorrecta): # loop que se ejecuta hasta que palabraCorrecta sea True
+     print('>>> INTRODUCIR PALABRA DE 5 LETRAS')
+     palabra = input() # variable de input
+     errors = []
+     if len(palabra) is not 5: # verifica si la palabra no tiene 5 caracteres
+        errors.append('NO ES DE 5 LETRAS')
+     if palabra.isalpha() == False: # verifica si la palabra/String son letras
+        errors.append('CONTIENE CARACTERES NO VALIDOS')
+     if (palabra not in diccionario): # verifica si la palabra esta en el diccionario
+        errors.append('NO ESTA EN EL DICCIONARIO')
+     if len(errors) is 0: # si la lista de errores esta vacia ejecuta 2 operaciones
+        print('>>> OPERACION COMPLETADA CON EXITO')
+        palabraCorrecta = True # termina el loop/while
+     else:
+        print('>>> ERROR: La palabra introducida '+','.join(errors)+'. Por favor vuelva a ingresar una palabra')
